87974041f6126b619908d36d2a6ed8442e55eee4
StarbuckBarista <carlitos.salazar@protonmail.com>
2023-06-25 18:50:08 -0500

    Removed Existing Help Implementation
    Deleted the dysfunctional `/pathapi help` implementation from the Cloud Command Framework.

--- src/main/java/com/tcc/pathfinderapi/PathFinderAPI.java
+++ src/main/java/com/tcc/pathfinderapi/PathFinderAPI.java
@@ -1,7 +1,6 @@
 package com.tcc.pathfinderapi;
 
 import cloud.commandframework.CommandManager;
-import cloud.commandframework.arguments.standard.StringArgument;
 import cloud.commandframework.bukkit.BukkitCommandManager;
 import cloud.commandframework.execution.CommandExecutionCoordinator;
 import cloud.commandframework.extra.confirmation.CommandConfirmationManager;
@@ -14,7 +13,6 @@ import com.tcc.pathfinderapi.messaging.PathAPIMessager;
 import com.tcc.pathfinderapi.pathing.BlockManager;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -59,27 +57,6 @@ public final class PathFinderAPI extends JavaPlugin {
                 .withCommandExecutionHandler()
                 .withDecorator(message -> Component.text("[PathAPI]").append(Component.space()).append(message))
                 .apply(commandManager, this.adventure::sender);
-
-            MinecraftHelp<CommandSender> minecraftHelp = new MinecraftHelp<CommandSender>(
-                "/pathapi help",
-                this.adventure::sender,
-                commandManager
-            );
-
-            minecraftHelp.setHelpColors(MinecraftHelp.HelpColors.of(
-                TextColor.color(0, 255, 0),
-                TextColor.color(0, 255, 255),
-                TextColor.color(255, 255, 0),
-                TextColor.color(120, 255, 120),
-                TextColor.color(200, 0, 200))
-            );
-
-            commandManager.command(
-                commandManager.commandBuilder("pathapi")
-                    .literal("help")
-                    .argument(StringArgument.optional("query", StringArgument.StringMode.GREEDY))
-                    .handler(context -> { minecraftHelp.queryCommands(context.getOrDefault("query", ""), context.getSender()); })
-            );
 
             CommandConfirmationManager<CommandSender> commandConfirmationManager = new CommandConfirmationManager<>(
                 30L,
