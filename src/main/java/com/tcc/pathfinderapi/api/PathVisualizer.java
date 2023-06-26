7d995f454fcba63833d44f5fa8faa97b261f90f9
StarbuckBarista <carlitos.salazar@protonmail.com>
2023-06-25 19:33:37 -0500

    Established Basic PathVisualizer Interface
    Established a preliminary estimate of the needed components for the PathVisualizer Interface. This first piece of code includes methods to initialize and clean full-length paths, as well as maintain relative path areas, as was discussed in #4.

---
+++ src/main/java/com/tcc/pathfinderapi/api/PathVisualizer.java
@@ -0,0 +1,42 @@
+package com.tcc.pathfinderapi.api;
+
+import java.util.LinkedList;
+
+import org.bukkit.entity.Player;
+
+import com.tcc.pathfinderapi.objects.Coordinate;
+
+interface PathVisualizer {
+    
+    /**
+     * Called when the path is first generated.
+     * Should be used to prepare the player/path for the journey, as well as initialize any data structures.
+     * @param player The player who is being shown the path.
+     * @param fullPath The full path that was generated.
+     */
+    public void initalizePath (Player player, LinkedList<Coordinate> fullPath);
+
+    /**
+     * Called when the player passes an old relative area of the path.
+     * Should be used to update the player/path from the old area, as well as update any data structures.
+     * @param player The player who is being shown the path.
+     * @param relativePath The old relative path that was generated.
+     */
+    public void interpretOldPath (Player player, LinkedList<Coordinate> relativePath);
+
+    /**
+     * Called when the player reaches a new relative area of the path.
+     * Should be used to update the player/path for the new area, as well as update any data structures.
+     * @param player The player who is being shown the path.
+     * @param relativePath The new relative path that was generated.
+     */
+    public void interpretNewPath (Player player, LinkedList<Coordinate> relativePath);
+
+    /**
+     * Called when the player has completed the path.
+     * Should be used to remove any lasting effects on the player/path, as well as clear any data structures.
+     * @param player The player who is being shown the path.
+     * @param fullPath The full path that was generated.
+     */
+    public void clearPath (Player player, LinkedList<Coordinate> fullPath);
+}
